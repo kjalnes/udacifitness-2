114ce06db671b6a8c6c39648f6632de4591e93c8
Kristine <kristine@Kristines-MacBook-Pro.local>
2017-12-21 10:36:31 +0100

    package-json removed

--- components/History.js
+++ components/History.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { connect } from 'react-redux';
-import { receiveEntries, addEntry } from '../actions';
-import { timeToString, getDailyReminderValue } from '../utils/helpers';
-import { fetchCalendarResults } from '../utils/api';
-// import UdaciFitnessCalendar from 'udacifitness-calendar';
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native'
+import { connect } from 'react-redux'
+import { receiveEntries, addEntry } from '../actions'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import { fetchCalendarResults } from '../utils/api'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+import { white } from '../utils/colors'
+import DateHeader from './DateHeader'
+import MetricCard from './MetricCard'
 
 class History extends Component {
   componentDidMount () {
@@ -21,15 +24,65 @@ class History extends Component {
       })
       .then(() => this.setState(() => ({ready: true})))
   }
-  render() {
-    console.log('this.props.entries',this.props.entries)
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View style={styles.item}>
+      {today
+        ? <View>
+              <DateHeader date={formattedDate} />
+              <Text style={styles.noDataText}>{today}</Text>
+          </View>
+        : <TouchableOpacity onPress={()=> console.log('Pressed')}>
+            <MetricCard metrics={metrics} date={formattedDate} />
+          </TouchableOpacity>}
+    </View>
+  )
+  renderEmptyDate(formattedDate) {
     return (
-      <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate} />
+        <Text style={styles.noDataText}>
+          You didnt log any data today.
+        </Text>
       </View>
     )
   }
+  render() {
+    const { entries } = this.props
+
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+})
+
 
 function mapStateToProps (entries) {
   return {
